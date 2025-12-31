f9ac76c76d74cd4d29a3ccbda73710d834cdfdf4
jcarlosxyz <jcarlosxyz@gmail.com>
2025-12-30 22:04:23 -0600

    Antes de la carga por lotes

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-responce.interfaces';
 
 @Injectable()
 export class SeedService {
 
 async executeSeed(){
     // URL de la API o recurso
-      const apiURL = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'; // Reemplaza con la URL que deseas consultar
+      const apiURL: string = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'; // Reemplaza con la URL que deseas consultar
 
       // Usar fetch para realizar una solicitud GE
 try {
@@ -14,20 +15,15 @@ try {
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        const data = await response.json();
+        const data: PokeResponse = await response.json();
+        //console.log(data.results[0]);
+        
         data.results.forEach(({name,url}) =>{
-
-              //console.log({name,url})
         const segment = url.split('/');
         console.log(segment);
         const no = +segment[segment.length - 2];
         console.log({name,no});
-
-        //datos de entrada 
-
         })
-        //console.log(data.results);
-
         return data.results;
     } catch (error) {
         console.error('Error fetching data:', error);
